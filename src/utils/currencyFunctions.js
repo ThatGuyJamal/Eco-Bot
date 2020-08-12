780cb6122a7ae328c7ab908372b0d7363491a59e
gavserve <62964708+gavserve@users.noreply.github.com>
2020-08-12 15:35:47 -0400

    some updates

--- src/utils/currencyFunctions.js
+++ src/utils/currencyFunctions.js
@@ -6,6 +6,15 @@ class currencyFunctions {
      * @param {string} [userId] - A Discord user ID
      * @param {striing} [guildId] - A Discord guild ID
      * @param {string} [amount] - Amount of coins to give
+     * 
+     * @example 
+     * const currencyFunctions = require('../utils/currencyFunctions');
+     * 
+     * bot.on('message', async message => {
+     *      const random = Math.floor(Math.random() * 24) + 1;
+     * 
+     *      giveCoins(message.author.id, message.guild.id, random);
+     * });
      */
 
     static async giveCoins(userId, guildId, amount) {
@@ -22,6 +31,13 @@ class currencyFunctions {
      * 
      * @param {string} [userId] - A Discord user ID
      * @param {string} [guildId] - A Discord guild ID
+     * 
+     * @example
+     * const currencyFunctions = require('../utils/currencyFunctions');
+     * 
+     * let data = await currencyFunctions.findUser(some_id, some_guild_id);
+     * 
+     * console.log(data);
      */
 
     static async findUser(userId, guildId) {
@@ -35,6 +51,13 @@ class currencyFunctions {
      * @param {string} [userId] - A Discord user ID
      * @param {string} [guildId] - A Discord guild ID
      * @param {number} [amount] - Amount of bank space to give
+     * 
+     * @example
+     * const currencyFunctions = require('../utils/currencyFunctions');
+     * 
+     * const random = Math.floor(Math.random() * 24) + 1;
+     * 
+     * currencyFunctions.giveBankSpace(some_id, some_guild_id, random);
      */
 
     static async giveBankSpace(userId, guildId, amount) {
@@ -51,6 +74,11 @@ class currencyFunctions {
      * 
      * @param {string} [userId] - A Discord user ID
      * @param {string} [guildId] - A Discord guild ID
+     * 
+     * @example
+     * const currencyFunctions = require("../utils/currencyFunctions");
+     * 
+     * currencyFunctions.removeAllCoins(some_id, some_guild_id);
      */
 
     static async removeAllCoins(userId, guildId) {
